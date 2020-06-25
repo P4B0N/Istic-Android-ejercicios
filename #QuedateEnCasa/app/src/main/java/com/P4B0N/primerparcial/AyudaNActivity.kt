c1c21b7e2334c484c070cd9de664161d77d07ac1
P4B0N <pbonaduce@iiconcepcion.edu.ar>
2020-06-24 21:21:24 -0300

    trabajando en las ayudas

---
+++ #QuedateEnCasa/app/src/main/java/com/P4B0N/primerparcial/AyudaNActivity.kt
@@ -0,0 +1,11 @@
+package com.P4B0N.primerparcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AyudaNActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ayuda_n)
+    }
+}
